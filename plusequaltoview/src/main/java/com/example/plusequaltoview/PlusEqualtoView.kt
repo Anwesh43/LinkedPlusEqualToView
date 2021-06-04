5a31e603dcef1e290f71e50724aea974101fdef3
Anwesh43 <anweshthecool0@gmail.com>
2021-06-04 16:36:06 +0530

    ading PlusEqualTo class

--- plusequaltoview/src/main/java/com/example/plusequaltoview/PlusEqualtoView.kt
+++ plusequaltoview/src/main/java/com/example/plusequaltoview/PlusEqualtoView.kt
@@ -180,4 +180,27 @@ class PlusEqualToView(ctx : Context) :View(ctx) {
             return this
         }
     }
+
+    data class PlusEqualTo(var i : Int) {
+
+        private var curr : PETNode = PETNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
