faa72c59b9fad26925128023688658dff72a71eb
Anwesh43 <anweshthecool0@gmail.com>
2021-06-04 15:49:47 +0530

    adding constants

--- plusequaltoview/src/main/java/com/example/plusequaltoview/PlusEqualtoView.kt
+++ plusequaltoview/src/main/java/com/example/plusequaltoview/PlusEqualtoView.kt
@@ -1,2 +1,27 @@
 package com.example.plusequaltoview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val strokeFactor : Float = 90f
+val rot : Float = 90f
+val gapFactor : Float = 11.2f
