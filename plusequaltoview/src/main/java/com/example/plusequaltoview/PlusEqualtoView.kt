45b6cd878b225bd3ef948892d42125c87f9c5987
Anwesh43 <anweshthecool0@gmail.com>
2021-06-04 16:18:40 +0530

    adding PlusEqualToView class

--- plusequaltoview/src/main/java/com/example/plusequaltoview/PlusEqualtoView.kt
+++ plusequaltoview/src/main/java/com/example/plusequaltoview/PlusEqualtoView.kt
@@ -76,3 +76,19 @@ fun Canvas.drawPETNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPlusEqualTo(scale, w, h, paint)
 }
+
+class PlusEqualToView(ctx : Context) :View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
